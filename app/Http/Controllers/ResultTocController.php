6132352c876ca2e961a6c694ffe3b90cbe7e0b87
dbxdevz <balginbek.balgin97@gmail.com>
2023-03-15 15:34:51 +0600

    fixed ResultTocController.php

--- app/Http/Controllers/ResultTocController.php
+++ app/Http/Controllers/ResultTocController.php
@@ -38,7 +38,7 @@ class ResultTocController extends Controller
         ]);
 
         $balanceTest = BalanceTest::where('id', $request->balance_test_id)->first();
-        if ($request->first_error != null){
+        if ($request->first_error !== null){
             $balanceTest->update([
                 'first_error' => $request->first_error
             ]);
@@ -54,16 +54,16 @@ class ResultTocController extends Controller
     public function incomeError(Request $request)
     {
         $request->validate([
-            'first_error' => ['nullable'],
+            'first_error' => ['required_if:second_error,=,null'],
             'second_error' => ['required_if:first_error,=,null'],
-            'income_test_id' => ['required'],
+            'income_test_id' => ['required', 'exists:income_tests,id'],
             'nature_control_id' => [
                 'exists:nature_controls,id',
                 new NatureControlRule(natureControlID: $request->nature_control_id, incomeID: $request->income_test_id)]
             ]);
 
             $incomeTest = IncomeTest::where('id', $request->income_test_id)->first();
-            if ($request->first_error != null){
+            if ($request->first_error !== null){
                 $incomeTest->update([
                     'first_error' => $request->first_error
                 ]);
